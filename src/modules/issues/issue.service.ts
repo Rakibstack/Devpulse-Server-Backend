3782fe031ea50a90ee59282b131294793605f37e
Rakibstack <rh6039511@gmail.com>
2026-06-13 16:39:54 +0600

    add global error handler

--- src/modules/issues/issue.service.ts
+++ src/modules/issues/issue.service.ts
@@ -10,8 +10,6 @@ const createIssueIntoDB = async (payload: Issues, id: string) => {
   }
   const result = await pool.query(
     `
-    
-
     INSERT INTO issues(title,description,type,status,reporter_id)  
     VALUES($1,$2,$3,COALESCE($4,'open'),$5) RETURNING * 
     `,
